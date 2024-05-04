9b69dd27b8740fe8de4cdd551174afd290d21cf8
anvarbek009 <anvarasqarov2009@gmail.com>
2024-05-04 21:15:21 +0500

    first commit

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'categories'
+    def __str__(self):
+        return self.name
+    
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    descriptions = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image=models.ImageField(upload_to='products/',blank=True, null=True)
+    
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return self.name
---
+++ products/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from .models import Category, Products
+from .form import ProductForm
+# Create your views here.
+
+def get_info(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'index.html', context=context)
+
+def get_products(request, pk):
+    products = Products.objects.filter(category=pk)
+    context = {
+        'products': products
+    }
+    return render(request, 'products.html', context=context)
+
+def detail(request, pk):
+    product = Products.objects.get(pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'detail.html', context=context)
+
+def add_products(request):
+    form = ProductForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('products:get_info')
+    context = {
+        'form': form
+        }
+    return render(request, 'create.html', context=context)
+
+
+def update_products(request, pk):
+    data = Products.objects.get(pk=pk)
+    form = ProductForm(request.POST, request.FILES, instance=data)
+    if form.is_valid():
+        print(1)
+        form.save()
+        return redirect('products:get_info')
+    context = {
+        'form': form
+        }
+    return render(request, 'update.html', context=context)
+
+def delete_products(request, pk):
+    data = Products.objects.get(pk=pk)
+    data.delete()
+    return redirect('products:get_info')
+
+def search_products(request):
+    if request.method == 'POST':
+        search = request.POST['search']
+        products = Products.objects.filter(name__icontains=search)
+        context = {
+            'products': products
+        }
+        return render(request, 'search.html', context=context)
+    else:
+        return redirect('products:get_info')
